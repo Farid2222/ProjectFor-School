228e9bfdc8eb895d8f3bba49f376eb941c4388be
Farid2222 <72570153+Farid2222@users.noreply.github.com>
2020-10-09 19:03:28 +0500

    #5
    скрипт: реализация числовых кнопок и кнопок арифметических действий (кроме равно)

--- calc.js
+++ calc.js
@@ -1,17 +1,30 @@
-let okno, a, b, result;
+let okno, result, curOp="";
 okno = document.getElementById("display");
+
+
 
 function Operation(op)
 {
-
+    // если последний символ в поле - цифра, то добавляем знак арифметической операции
+    if (parseInt(okno.value[okno.value.length - 1]) >= 0 && parseInt(okno.value[okno.value.length - 1]) <= 9)
+    {
+        okno.value += " " + op + " ";
+    }
 }
 
 function SetNumber(num)
 {
-    
+    // если в поле значение 0, обновляем поле
+    if (parseInt(okno.value) == 0)
+    {
+        okno.value = num;
+        return;
+    }
+    okno.value += num;
 }
 
 function Clear()
 {
-    okno.value = "";
+    okno.value = "0";
+    result = 0;
 }
