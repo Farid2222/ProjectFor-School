b4dbcd1ecda8f4e4263447a90a46e3200cd578a5
Farid2222 <72570153+Farid2222@users.noreply.github.com>
2020-10-09 15:18:06 +0500

    #4
    html css: увеличение ширины блока калькулятора до 400 px, добавление кнопок арифметических действий
    скрипт: добавление функций для арифметических действий и очистки поля вывода

--- calc.js
+++ calc.js
@@ -1,9 +1,17 @@
 let okno, a, b, result;
 okno = document.getElementById("display");
 
+function Operation(op)
+{
 
+}
 
 function SetNumber(num)
 {
     
+}
+
+function Clear()
+{
+    okno.value = "";
 }
